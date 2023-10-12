9d20b87e052c5e8b61f58c19ad15749f57efe0c9
f <snake386@hotmail.com>
2023-10-11 20:04:37 -0400

    add startup-nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,12 @@
 -- You can add your own plugins here or in other files in this directory!
 return {
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+
+    {
+        "startup-nvim/startup.nvim",
+        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+        config = function()
+            require "startup".setup()
+        end
+    }
 }
