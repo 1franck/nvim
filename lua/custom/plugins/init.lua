293355871c5a8fe4f52bf3c26e1c9b8939d15764
f <snake386@hotmail.com>
2023-10-12 01:40:26 -0400

    indentation + shortcut for projects

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,13 +1,27 @@
 -- You can add your own plugins here or in other files in this directory!
 
 return {
+    -- themes
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
+    -- startup
     {
         "startup-nvim/startup.nvim",
         requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function()
             require "startup".setup(require "configs.startup_nvim")
         end
-    }
+    },
+
+    -- project
+    {
+        "ahmedkhalf/project.nvim",
+        config = function()
+            require("project_nvim").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    },
 }
