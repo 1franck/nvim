cb691489e2846c01bb6f5818b3950ae5bc28bfdb
f <snake386@hotmail.com>
2023-10-11 21:23:03 -0400

    move startup default theme to configs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,4 +1,5 @@
 -- You can add your own plugins here or in other files in this directory!
+
 return {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
@@ -6,7 +7,7 @@ return {
         "startup-nvim/startup.nvim",
         requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function()
-            require "startup".setup()
+            require "startup".setup(require "configs.startup_nvim")
         end
     }
 }
