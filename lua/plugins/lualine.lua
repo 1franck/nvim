72d1783b87fafde08383238f613e530e5ae08a49
f <snake386@hotmail.com>
2023-10-14 19:20:13 -0400

    more more and more

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,8 +2,9 @@ return {
     {
         -- Set lualine as statusline
         'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true },
-        -- See `:help lualine.txt`
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        },
         opts = {
             options = {
                 icons_enabled = true,
--- lua/plugins/startup.lua
+++ lua/plugins/startup.lua
@@ -4,7 +4,6 @@ return {
         dependencies = {
             'nvim-lua/plenary.nvim',
         },
-        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
         config = function()
             require "startup".setup(require "plugins.configs.startup")
         end
