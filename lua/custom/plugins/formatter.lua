b20d2dff1a81a7368181ed0d66d7a5e9814d4d8a
f <snake386@hotmail.com>
2023-10-14 18:53:37 -0400

    cleanup

--- lua/custom/plugins/formatter.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "mhartington/formatter.nvim",
-        config = function()
-            require("configs.formatter_nvim")
-            vim.keymap.set('n', '<leader>f', ":Format<CR>", { desc = 'Format' })
-        end
-    }
-}
