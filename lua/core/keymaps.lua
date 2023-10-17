f59904a5158b8f8b6a4e4662d30f0afa3014b7eb
f <snake386@hotmail.com>
2023-10-16 23:43:21 -0400

    add keymap for current buzzer fuzzy find

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -17,6 +17,7 @@ vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { des
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<C-f>', ':lua require("telescope.builtin").current_buffer_fuzzy_find({ sorter = require("telescope.sorters").get_substr_matcher({})})<cr>', { desc = "Current buffer fuzzy find" })
 
 -- nvim-tree 
 vim.keymap.set('n', "<C-n>", ":NvimTreeToggle<CR>", { desc = 'Toggle NvimTree' })
