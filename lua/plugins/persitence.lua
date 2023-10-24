8dd32ea475287296d626fbd86ae06e40a9b742a3
f <snake386@hotmail.com>
2023-10-24 09:07:58 -0400

    persistence plugin

---
+++ lua/plugins/persitence.lua
@@ -0,0 +1,14 @@
+-- https://github.com/folke/persistence.nvim
+return {
+    {
+        "folke/persistence.nvim",
+        event = "BufReadPre", -- this will only start session saving when an actual file was opened
+        module = "persistence",
+        config = function()
+            require("persistence").setup {
+                dir = vim.fn.expand(vim.fn.stdpath "config" .. "/session/"),
+                options = { "buffers", "curdir", "tabpages", "winsize" },
+            }
+        end,
+    },
+}
